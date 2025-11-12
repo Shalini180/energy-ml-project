1b8aec46748da2d5fba0fa8c45a11c090ab6f436
unknown <shalini.a.annadurai@gmail.com>
2025-11-12 09:36:04 +0530

    docs: baseline README with roadmap and workflow

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,4 +1,12 @@
-﻿from src.utils.query_parser import QueryAnalyzer
+﻿import sys
+import os
+
+# Add the absolute path to the 'src' directory to sys.path
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
+)
+
+from utils.query_parser import QueryAnalyzer
 
 
 def test_analyzer_smoke():
